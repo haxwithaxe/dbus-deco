ea7de72a1985fdd7e1a17fa2e9f38ffaac1dd348
haxwithaxe <me@haxwithaxe.net>
2016-07-17 11:17:13 -0400

    added dbus property decorator workalike. addded setup.py

--- dbus_deco/__init__.py
+++ dbus_deco/__init__.py
@@ -18,7 +18,7 @@ def fix_class_name(klass, name):
     klass.__qualname__ = '.'.join(qualname)
 
 
-class DBusProperty:
+class DBusClientProperty:
     """A @property work-alike for DBus methods.
     This is based on `PyProperty_Type()` in `Objects/descrobject.c` as interpreted by Raymond Hettinger on
     http://stackoverflow.com/questions/12405087/subclassing-pythons-property
@@ -122,3 +122,76 @@ class DBusProperty:
                 return fdel(obj, __deleter())
             return type(self)(self.fget, self.fset, deleter_wrapper, self.__doc__, self.name, self.interface)
         return deleter_decorator
+
+
+
+class DBusServiceProperty:
+    """A @property work-alike for DBus methods.
+    This is based on `PyProperty_Type()` in `Objects/descrobject.c` as interpreted by Raymond Hettinger on
+    http://stackoverflow.com/questions/12405087/subclassing-pythons-property
+
+    Attributes:
+        fget (callable): The function or method to use to get the value.
+        fset (callable): The function or method to use to set the value.
+        fdel (callable): The function or method to use to delete the value.
+        name (str): The default DBus service method name (eg "get_message" of
+            "com.example.service.Message.get_message"). This is used as the
+            method name if none is specified in the getter, setter, or deleter
+            method calls.
+        interface (str): The default DBus service interface name (eg "Message"
+            of "com.example.service.Message"). This is used as the method name
+            if none is specified in the getter, setter, or deleter method calls.
+
+
+    Arguments:
+        fget (callable): The function or method to use to get the value.
+        fset (callable, optional): The function or method to use to set the
+            value. Defaults to None.
+        fdel (callable, optional): The function or method to use to delete the
+            value. Defaults to None.
+        doc (str, optional): The docstring for the property. Defaults to
+            `fget.__doc__`.
+        name (str, optional): The DBus service method name (eg "get_message" of
+            "com.example.service.Message.get_message"). Defaults to None.
+        interface (str, optional): The DBus service interface name
+            (eg "Message" of "com.example.service.Message"). Defaults to None.
+
+    """
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None, diproperty=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        self.di = diproperty
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(obj, value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(obj)
+
+    def getter(self, fget):
+        self.di.readable = True
+        return type(self)(fget, self.fset, self.fdel, self.__doc__, self.di)
+
+    def setter(self, fset):
+        self.di.writeable = True
+        return type(self)(self.fget, fset, self.fdel, self.__doc__, self.di)
+
+    def deleter(self, fdel):
+        self.di.writeable = True
+        return type(self)(self.fget, self.fset, fdel, self.__doc__, self.di)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='dbus-deco',
+      version='0.1',
+      description='DBus helpers.',
+      author='haxwithaxe',
+      author_email='spam@haxwithaxe.net',
+      url='https://github.com/haxwithaxe/dbus-deco',
+      packages=['dbus_deco'],
+     )

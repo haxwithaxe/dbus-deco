2eab5c3802a1f79bc86073f218fc73348739a9b2
haxwithaxe <me@haxwithaxe.net>
2016-07-18 21:45:52 -0400

    pydbussified the service side

--- dbus_deco/service.py
+++ dbus_deco/service.py
@@ -1,91 +1,111 @@
 
 import logging
 
-import dbus
-import dbus.service
-import dbus.mainloop.glib
+import pydbus
 from gi.repository import GObject
+from lxml import etree
+from lxml.objectify import E
 
-from dbus_deco import DBusServiceProperty, extrapolate_service_path, fix_class_name
+from dbus_deco import fix_class_name
 
 
-def service_factory(name, path=None, class_name=None, class_doc=None, bus_class=None):
-        service_name = name
-        service_path = path or extrapolate_service_path(name)
+def _decorator(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
 
-        class ServiceBaseClass(dbus.service.Object):
 
-            name = service_name
-            path = service_path
-            bus = bus_class or dbus.SessionBus
+class Arg:
 
-            def __init__(slf):
-                slf.logger = logging.getLogger(
-                        name=slf.__class__.__name__+'.'+slf.name
-                        )
+    def __init__(self, name, type_='s', direction='in'):
+        self._name = name
+        self._type = type_
+        self._direction = direction
 
-            def run(slf):
-                dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-                bus_name = dbus.service.BusName(slf.name, slf.bus())
-                dbus.service.Object.__init__(slf, bus_name, slf.path)
-                slf._loop = GObject.MainLoop()
-                slf.logger.debug("Service running...")
-                slf._loop.run()
-                slf.logger.debug("Service stopped")
+    def __iter__(self):
+        return iter((('name', self._name), ('type', self._type), ('direction', self._direction)))
 
-            @classmethod
-            def _get_dbus_method_name(cls, name):
-                return '.'.join((cls.name, name))
+    @property
+    def __xml__(self):
+        return E.arg(**dict(self))
 
-            @classmethod
-            def method(cls, **kwargs):
-                return dbus.service.method(cls.name, **kwargs)
 
-            @classmethod
-            def attribute(cls, getter_name, *args, doc=None, **kwargs):
-                def attribute_decorator(func):
-                   return DBusServiceProperty(
-                            fget=func,
-                            name=getter_name,
-                            interface=cls._get_dbus_method_name(getter_name),
-                            doc=doc
-                            )
-                return attribute_decorator
+class Method:
 
-            @classmethod
-            def signal(cls, **kwargs):
-                return dbus.service.signal(cls.name, **kwargs)
+    def __init__(self, name, *args):
+        self._name = name
+        self._args = args
 
+    def __iter__(self):
+        return iter((('name', self._name),))
+
+
+class Signal:
+
+    def __init__(self, name, *args):
+        self._name = name
+        self._args = args
+
+    def __iter__(self):
+        return iter((('name', self._name),))
+
+    @property
+    def __xml__(self):
+        return E.signal(*[x.__xml__ for x in self._args], **dict(self))
+
+
+class Service:
+
+    name = None
+    children = []
+    
+    @classmethod
+    def factory(cls, service_name, class_name=None, class_doc=None):
+        class ServiceBaseClass(cls):
+            pass
+        ServiceBaseClass.name = service_name
         if class_name:
             fix_class_name(ServiceBaseClass, class_name)
         if class_doc:
             ServiceBaseClass.__doc__ = class_doc
         return ServiceBaseClass
 
+    @classmethod
+    def add_child(cls, child):
+        cls.children.append(child.__xml__)
+
+    @classmethod
+    def method(cls, name, *args, response=None):
+        if response:
+            args = list(args) + [Arg('response', response, 'out')]
+        cls.add_child(Method(name, *args))
+        return _decorator
+    
+    @classmethod
+    def signal(cls, name, *args, response=None):
+        return lambda _: signal
+
+    @classmethod
+    def tostring(cls):
+        return etree.tostring(E.node(E.interface(*cls.children, name=cls.name))).decode()
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    service = service_factory('com.example.service')
+    service = Service.factory('com.example.service')
 
     class ExampleService(service):
 
-        def __init__(self, message):
-            super().__init__()
-            self._message = message
-
-        @service.method()
-        def get_message(self):
-            self.message_ready(self._message)
-
-        @service.signal(signature='s')
-        def message_ready(self, message):
-            self.logger.debug("  sending message")
+        @service.method('HelloWorld', response='s')
+        def HelloWorld(self):
+            return 'Hello!'
+        
+        @service.method('Echo', Arg('message', 's'), response='s')
+        def Echo(self, message):
             return message
 
-        @service.method()
-        def quit(self):
-            self.logger.debug("  shutting down")
-            self._loop.quit()
 
-    
-    ExampleService("This is the service").run()
+    loop = GObject.MainLoop()
+    bus = pydbus.SessionBus()
+    bus.publish('com.example.service', ExampleService())
+    loop.run()
